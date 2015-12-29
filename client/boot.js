0870fc37b06b2c6a5d62b13a5892127cd0a8c29f
blacksonic <soos.gabor86@gmail.com>
2015-12-29 20:19:27 +0100

    add zone js for change detection

--- client/boot.js
+++ client/boot.js
@@ -1,6 +1,7 @@
 'use strict';
 import 'es6-shim';
 import 'reflect-metadata';
+import 'zone.js/lib/browser/zone-microtask';
 import {bootstrap} from 'angular2/platform/browser'
 import {AppComponent} from './app/app.component'
 
