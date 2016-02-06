323a5f1cb5519760aa8854d3979414c4d6497ab9
blacksonic <soos.gabor86@gmail.com>
2016-02-06 22:41:40 +0100

    fix testing

--- client/app/components/hello.spec.js
+++ client/app/components/hello.spec.js
@@ -1,19 +1,12 @@
 'use strict';
 import {HelloComponent} from './hello';
-//import {AsyncTestCompleter, TestComponentBuilder} from 'angular2/testing_internal';
+import {TestComponentBuilder} from 'angular2/testing';
 
 describe('AppCoponent', function() {
-  it('should works', function() {
-    expect(true).toBe(true);
-  });
-
-  //it('renders app', inject([TestComponentBuilder, AsyncTestCompleter], (builder, async) => {
-  //  builder.createAsync(HelloComponent)
-  //    .then((fixture) => {
-  //      expect(fixture.debugElement.nativeElement).toHaveText('Angular 2 App');
-  //
-  //      async.done();
-  //    })
-  //    .catch((e) => async.done(e));
-  //}));
+  it('renders app', injectAsync([TestComponentBuilder], (builder) => {
+    return builder.createAsync(HelloComponent)
+      .then((fixture) => {
+        expect(fixture.debugElement.nativeElement).toHaveText('Angular 2 App');
+      });
+  }));
 });
--- client/setup.spec.js
+++ client/setup.spec.js
@@ -5,28 +5,32 @@ import 'reflect-metadata';
 import 'zone.js/dist/zone-microtask';
 import 'zone.js/dist/jasmine-patch';
 
-import {setBaseTestProviders} from 'angular2/testing';
+import {BrowserDomAdapter} from 'angular2/src/platform/browser/browser_adapter';
 import {TEST_BROWSER_PLATFORM_PROVIDERS, TEST_BROWSER_APPLICATION_PROVIDERS} from 'angular2/platform/testing/browser';
+import {setBaseTestProviders} from 'angular2/testing';
 setBaseTestProviders(TEST_BROWSER_PLATFORM_PROVIDERS,TEST_BROWSER_APPLICATION_PROVIDERS);
 
-// TODO: testing is broken from beta.2
-//import {
-//  afterEach,
-//  beforeEach,
-//  beforeEachProviders,
-//  describe,
-//  expect,
-//  inject,
-//  it
-//} from 'angular2/testing_internal';
-//
-//global.expect = expect;
-//global.inject = inject;
-//global.describe = describe;
-//global.it = it;
-//global.beforeEach = beforeEach;
-//global.beforeEachProviders = beforeEachProviders;
-//global.afterEach = afterEach;
+BrowserDomAdapter.makeCurrent();
+
+import {
+  afterEach,
+  beforeEach,
+  beforeEachProviders,
+  describe,
+  expect,
+  inject,
+  injectAsync,
+  it
+} from 'angular2/testing';
+
+global.expect = expect;
+global.inject = inject;
+global.injectAsync = injectAsync;
+global.describe = describe;
+global.it = it;
+global.beforeEach = beforeEach;
+global.beforeEachProviders = beforeEachProviders;
+global.afterEach = afterEach;
 
 let testContext = require.context('./app', true, /\.spec\.js/);
 testContext.keys().forEach(testContext);
