1657f29f2ec1232eb3372974d8b87c9a0bfb8eb3
blacksonic <soos.gabor86@gmail.com>
2016-02-06 22:13:06 +0100

    modify test setup

--- client/setup.spec.js
+++ client/setup.spec.js
@@ -5,6 +5,10 @@ import 'reflect-metadata';
 import 'zone.js/dist/zone-microtask';
 import 'zone.js/dist/jasmine-patch';
 
+import {setBaseTestProviders} from 'angular2/testing';
+import {TEST_BROWSER_PLATFORM_PROVIDERS, TEST_BROWSER_APPLICATION_PROVIDERS} from 'angular2/platform/testing/browser';
+setBaseTestProviders(TEST_BROWSER_PLATFORM_PROVIDERS,TEST_BROWSER_APPLICATION_PROVIDERS);
+
 // TODO: testing is broken from beta.2
 //import {
 //  afterEach,
@@ -26,11 +30,3 @@ import 'zone.js/dist/jasmine-patch';
 
 let testContext = require.context('./app', true, /\.spec\.js/);
 testContext.keys().forEach(testContext);
-
-//import {setBaseTestProviders} from 'angular2/testing';
-//import {
-//  TEST_BROWSER_PLATFORM_PROVIDERS,
-//  TEST_BROWSER_APPLICATION_PROVIDERS
-//} from 'angular2/platform/testing/browser';
-//setBaseTestProviders(TEST_BROWSER_PLATFORM_PROVIDERS,
-//  TEST_BROWSER_APPLICATION_PROVIDERS);
