a5a21ffb8701f265c3050cfadc36028b8588a3e3
blacksonic <soos.gabor86@gmail.com>
2016-03-18 22:19:06 +0100

    feat(package): upgrade to beta.10

--- client/shim.js
+++ client/shim.js
@@ -1,4 +1,5 @@
 //import 'es6-shim';
 //import 'reflect-metadata';
 import 'core-js';
-import 'zone.js/dist/zone-microtask';
+import 'zone.js/dist/zone';
+import 'zone.js/dist/long-stack-trace-zone';
