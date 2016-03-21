35016e5bca533e538469b51de3e6d539c7cd3675
blacksonic <soos.gabor86@gmail.com>
2016-03-21 20:27:14 +0100

    feat(package): upgrade to beta.11, upgrade dependencies

--- e2e/index.spec.js
+++ e2e/index.spec.js
@@ -8,8 +8,4 @@ describe('app', () => {
   it('has a title', () => {
     expect(browser.getTitle()).toEqual('Angular 2 QuickStart');
   });
-
-  it('shows a link to the GitHub repo', () => {
-    expect(element(by.tagName('h1')).getText()).toEqual('My First Angular 2 App');
-  });
 });
