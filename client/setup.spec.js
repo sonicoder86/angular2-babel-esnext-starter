a2d3466a91975c2aa9033493d487192f1d081ebb
blacksonic <soos.gabor86@gmail.com>
2016-03-26 21:05:45 +0100

    feat(test): remove duplicate import

--- client/setup.spec.js
+++ client/setup.spec.js
@@ -2,16 +2,18 @@ import 'phantomjs-polyfill';
 import './shim';
 import 'zone.js/dist/jasmine-patch';
 
-import { TEST_BROWSER_PLATFORM_PROVIDERS, TEST_BROWSER_APPLICATION_PROVIDERS } from 'angular2/platform/testing/browser';
-import { setBaseTestProviders } from 'angular2/testing';
-setBaseTestProviders(TEST_BROWSER_PLATFORM_PROVIDERS, TEST_BROWSER_APPLICATION_PROVIDERS);
-
 import {
+  TEST_BROWSER_PLATFORM_PROVIDERS, TEST_BROWSER_APPLICATION_PROVIDERS
+} from 'angular2/platform/testing/browser';
+import {
+  setBaseTestProviders
   afterEach, beforeEach, beforeEachProviders,
   describe, it, expect,
   inject, injectAsync,
   fakeAsync, tick
 } from 'angular2/testing';
+
+setBaseTestProviders(TEST_BROWSER_PLATFORM_PROVIDERS, TEST_BROWSER_APPLICATION_PROVIDERS);
 
 Object.assign(global, {
   afterEach, beforeEach, beforeEachProviders,
