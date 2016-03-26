db30ac67891cc277b9259b93ec57d1c5af337a10
blacksonic <soos.gabor86@gmail.com>
2016-03-26 21:03:30 +0100

    feat(test): remove core-js, test setup improvement

--- client/setup.spec.js
+++ client/setup.spec.js
@@ -7,28 +7,20 @@ import { setBaseTestProviders } from 'angular2/testing';
 setBaseTestProviders(TEST_BROWSER_PLATFORM_PROVIDERS, TEST_BROWSER_APPLICATION_PROVIDERS);
 
 import {
-  afterEach,
-  beforeEach,
-  beforeEachProviders,
-  describe,
-  expect,
-  inject,
-  injectAsync,
-  it,
-  fakeAsync,
-  tick
+  afterEach, beforeEach, beforeEachProviders,
+  describe, it, expect,
+  inject, injectAsync,
+  fakeAsync, tick
 } from 'angular2/testing';
 
-global.expect = expect;
-global.inject = inject;
-global.injectAsync = injectAsync;
-global.describe = describe;
-global.it = it;
-global.beforeEach = beforeEach;
-global.beforeEachProviders = beforeEachProviders;
-global.afterEach = afterEach;
-global.fakeAsync = fakeAsync;
-global.tick = tick;
+Object.assign(global, {
+  afterEach, beforeEach, beforeEachProviders,
+  describe, it, expect,
+  inject, injectAsync,
+  fakeAsync, tick
+});
+
+Error.stackTraceLimit = Infinity;
 
 let testContext = require.context('./app', true, /\.spec\.js/);
 testContext.keys().forEach(testContext);
--- client/shim.js
+++ client/shim.js
@@ -1,5 +1,6 @@
-//import 'es6-shim';
-//import 'reflect-metadata';
-import 'core-js';
+import 'es6-shim';
+import 'es6-promise';
+import 'reflect-metadata';
+
 import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
