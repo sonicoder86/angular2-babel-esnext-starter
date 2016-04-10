b6ff3cdd0f03f0f1798522e57e5cde4b7a2314b7
blacksonic <soos.gabor86@gmail.com>
2016-04-10 11:20:41 +0200

    feat(about): cleanup test

--- client/app/core/components/about/about.spec.js
+++ client/app/core/components/about/about.spec.js
@@ -1,32 +1,23 @@
 import { TestComponentBuilder } from 'angular2/testing';
-import { BrowserDomAdapter } from 'angular2/platform/browser';
 import { AboutComponent } from './about';
 
 describe('AboutComponent', () => {
-  let subjectFixture;
-  let subject;
   let subjectElement;
-  let dom;
 
   beforeEachProviders(() => [
-    TestComponentBuilder,
-    BrowserDomAdapter
+    TestComponentBuilder
   ]);
 
-  beforeEach(injectAsync([TestComponentBuilder, BrowserDomAdapter], (componentBuilder, _dom_) => {
-    dom = _dom_;
-
+  beforeEach(injectAsync([TestComponentBuilder], (componentBuilder) => {
     return componentBuilder
       .createAsync(AboutComponent)
       .then(fixture => {
-        subject = fixture.componentInstance;
         subjectElement = fixture.nativeElement;
-        subjectFixture = fixture;
       });
   }));
 
   it('should display title', () => {
-    let header = dom.querySelector(subjectElement, 'h1');
+    let header = subjectElement.querySelector('h1');
 
     expect(header.textContent).toEqual('About');
   });
