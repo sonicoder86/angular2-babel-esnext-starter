2c2ce6aff2e3497eef8a7ff041358dbaae6e8a6e
shootermv <shootermv@gmail.com>
2016-06-16 16:41:58 +0300

    thumbnails added to list view

--- server/router.js
+++ server/router.js
@@ -15,12 +15,14 @@ let posts = [
     "_id": uuid.v4(),
     "name" : "angular2 - first step",
     "text" : getArticle('article_1.html'),
+    "img": "https://gravatar.com/avatar/c9e0eedda66ad0a55cb7aaced698edf2?s=96&amp;d=https://dashboard.heroku.com%2Fimages%2Fstatic%2Fninja-avatar-48x48.png",
    // "website" : "http://gs500coder.blogspot.co.il/2016/01/building-my-first-directive-with.html/",
     "description" : "Want to know how is it feeling programming Angular?"
   },
   {
     "_id": uuid.v4(),
     "name" : "Creating chart component - angular2",
+    "img": "https://gravatar.com/avatar/c9e0eedda66ad0a55cb7aaced698edf2?s=96&amp;d=https://dashboard.heroku.com%2Fimages%2Fstatic%2Fninja-avatar-48x48.png",
    // "website" : "http://gs500coder.blogspot.co.il/2016/01/creating-chart-component-angular2.html",
     "text" : getArticle('article_2.html'),   
     "description" : "create an angular2 component which displays chart"
@@ -28,13 +30,15 @@ let posts = [
   {
     "_id": uuid.v4(),
     "name" : "Making collapsible directive with angular2",
+    "img": "https://gravatar.com/avatar/c9e0eedda66ad0a55cb7aaced698edf2?s=96&amp;d=https://dashboard.heroku.com%2Fimages%2Fstatic%2Fninja-avatar-48x48.png",
     "text" : getArticle('article_3.html'), 
     //"website" : "http://gs500coder.blogspot.co.il/2016/02/bootstrap-collapsible-directive-in.html",
     "description" : "making collapsible directive based on bootstrap library"
   },
   {
-    "_id": uuid.v4(),
+    "_id": uuid.v4(),    
     "name" : "Using Angular2 Services",
+    "img": "https://gravatar.com/avatar/c9e0eedda66ad0a55cb7aaced698edf2?s=96&amp;d=https://dashboard.heroku.com%2Fimages%2Fstatic%2Fninja-avatar-48x48.png",    
     "text" : getArticle('article_4.html'), 
   // "website" : "http://gs500coder.blogspot.co.il/2016/02/using-angular2-services.html",
     "description" : "How to make Angular2 Services"
@@ -42,6 +46,7 @@ let posts = [
   {
     "_id": uuid.v4(),
     "name" : "Promises in Angular2",
+    "img": "https://gravatar.com/avatar/c9e0eedda66ad0a55cb7aaced698edf2?s=96&amp;d=https://dashboard.heroku.com%2Fimages%2Fstatic%2Fninja-avatar-48x48.png",    
     "text" : getArticle('article_5.html'), 
     //"website" : "http://gs500coder.blogspot.co.il/2016/03/using-promises-in-angular2.html",
     "description" : "What are Promises and how to use them with Angular2"
@@ -49,6 +54,7 @@ let posts = [
   {
     "_id": uuid.v4(),
     "name" : "Custom events in angular2",
+    "img": "https://gravatar.com/avatar/c9e0eedda66ad0a55cb7aaced698edf2?s=96&amp;d=https://dashboard.heroku.com%2Fimages%2Fstatic%2Fninja-avatar-48x48.png",
     "text" : getArticle('article_6.html'), 
     //"website" : "http://gs500coder.blogspot.co.il/2016/04/events-in-angular2.html",
     "description" : "What are custom events in Angular2"
@@ -56,6 +62,7 @@ let posts = [
   {
     "_id": uuid.v4(),
     "name" : "Routes in angular2 (part 1)",
+    "img": "https://gravatar.com/avatar/c9e0eedda66ad0a55cb7aaced698edf2?s=96&amp;d=https://dashboard.heroku.com%2Fimages%2Fstatic%2Fninja-avatar-48x48.png",            
     "text" : getArticle('article_7.html'),     
     //"website" : "http://gs500coder.blogspot.co.il/2016/05/nested-routes-challenge-in-angular2.html",
     "description" : "Routes in Angular2"
@@ -63,7 +70,8 @@ let posts = [
   {
     "_id": uuid.v4(),
     "name" : "Routes in angular2 (part 2)",
-    "text" : getArticle('article_8.html'),     
+    "text" : getArticle('article_8.html'),
+    "img": "https://gravatar.com/avatar/c9e0eedda66ad0a55cb7aaced698edf2?s=96&amp;d=https://dashboard.heroku.com%2Fimages%2Fstatic%2Fninja-avatar-48x48.png",       
     //"website" : "http://gs500coder.blogspot.co.il/2016/05/routes-in-angular2-part-2.html",
     "description" : "Nested Routes in Angular2"
   }
