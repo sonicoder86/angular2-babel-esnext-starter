389b98abfa6500950ab87d50bbb6b579693bcd66
blacksonic <soos.gabor86@gmail.com>
2016-08-28 11:43:19 +0200

    upgrade packages and fix zonejs setup in tests

--- client/test.vendor.js
+++ client/test.vendor.js
@@ -1,7 +1,9 @@
 import 'phantomjs-polyfill';
 import './shim';
-import 'zone.js/dist/jasmine-patch';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
+import 'zone.js/dist/sync-test';
+import 'zone.js/dist/proxy';
+import 'zone.js/dist/jasmine-patch';
 
 Error.stackTraceLimit = Infinity;
