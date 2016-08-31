5e99f806ae1ded1a1ea8a8535ea184e03aeea079
blacksonic <soos.gabor86@gmail.com>
2016-08-31 13:04:47 +0200

    move spies to separate methods

--- client/app/auth/services/storage/storage.service.spec.js
+++ client/app/auth/services/storage/storage.service.spec.js
@@ -7,10 +7,6 @@ describe('StorageService', () => {
   let subject;
 
   beforeEach(() => {
-    spyOn(localStorage, 'getItem').and.returnValue('secret_token');
-    spyOn(localStorage, 'setItem');
-    spyOn(localStorage, 'removeItem');
-
     TestBed.configureTestingModule({
       providers: [StorageService]
     });
@@ -21,17 +17,23 @@ describe('StorageService', () => {
   }));
 
   it('should get auth token', () => {
+    spyOn(localStorage, 'getItem').and.returnValue('secret_token');
+
     expect(subject.getAuthToken()).toEqual('secret_token');
     expect(localStorage.getItem).toHaveBeenCalledWith('auth_token');
   });
 
   it('should set auth token', () => {
+    spyOn(localStorage, 'setItem');
+
     subject.setAuthToken('new_token');
 
     expect(localStorage.setItem).toHaveBeenCalledWith('auth_token', 'new_token');
   });
 
   it('should remove auth token', () => {
+    spyOn(localStorage, 'removeItem');
+
     subject.removeAuthToken();
 
     expect(localStorage.removeItem).toHaveBeenCalledWith('auth_token');
